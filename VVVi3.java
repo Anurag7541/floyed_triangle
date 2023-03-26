9e7756b01906de5430f2b539239e75ba8165c60c
Anurag kumar <117309701+Anurag7541@users.noreply.github.com>
2023-03-26 20:05:16 +0530

    Add files via upload

---
+++ VVVi3.java
@@ -0,0 +1,18 @@
+package Com;
+
+public class VVVi3 {
+    public static void main(String[] args) {
+        floyd_Triangle(5);
+    }
+
+    public static void floyd_Triangle(int n){
+        int counter = 1;
+        for(int i = 1;i<=n; i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
